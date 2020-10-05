750b675e5cdc3e8be5b321b3f5f3aaf59a2d8f57
islem talbi <islam.talbi@esprit.tn>
2020-10-05 16:53:36 +0100

    flutter mobile app

---
+++ android/app/src/main/kotlin/com/example/camp_with_us/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.camp_with_us
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:camp_with_us/login.dart';
+
+void main() => runApp(new MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Login(),
+));
